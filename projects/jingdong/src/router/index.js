efbe2204eab2bf367e21421f5e862ec9f0f7dd17
LHJ Piper <2940596912@qq.com>
2021-05-02 11:54:31 +0800

    vue proj jingdong router-guard

--- projects/jingdong/src/router/index.js
+++ projects/jingdong/src/router/index.js
@@ -1,6 +1,33 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
+import Home from '../views/home/Home.vue'
+import Login from '../views/login/Login.vue'
 
 const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login,
+    beforeEnter (to, from, next) {
+      // console.log(to, from)
+      // {fullPath: "/", hash: "", query: {…}, name: "Home", path: "/",…}
+      // {fullPath: "/login", hash: "", query: {…}, name: "Login", path: "/login",…}
+      // next()  // 调用 next 逻辑才会继续执行
+      // const isLogin = localStorage.isLogin
+      // if (isLogin) {
+      //   next({ name: 'Home' })
+      // } else {
+      //   next()
+      // }
+      // 上面的代码可以精简
+      const { isLogin } = localStorage
+      isLogin ? next({ name: 'Home' }) : next()
+    }
+  }
   // {
   //   path: '/',
   //   name: 'Home',
@@ -19,6 +46,21 @@ const routes = [
 const router = createRouter({
   history: createWebHashHistory(),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  // 在 Chrome 里的 Application 里看 local Storage
+  // const isLogin = localStorage.isLogin
+  // if (isLogin || to.name === 'Login') {
+  //   next()
+  // } else {
+  //   next({ name: 'Login' })
+  // }
+  // console.log(to, from)
+  // 上面的代码可以精简
+  const { isLogin } = localStorage;
+  (isLogin || to.name === 'Login') ? next() : next({ name: 'Login' })
+  next()
 })
 
 export default router
