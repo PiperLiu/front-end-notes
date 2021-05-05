22265010f5e07c24100ca5f934cec042821780b0
LHJ Piper <2940596912@qq.com>
2021-05-05 14:41:05 +0800

    vue proj jingdong local storage for cartList

--- projects/jingdong/src/store/index.js
+++ projects/jingdong/src/store/index.js
@@ -1,22 +1,23 @@
 import { createStore } from 'vuex'
 
+const setLocalCartList = (state) => {
+  const { cartList } = state
+  const cartListString = JSON.stringify(cartList)
+  localStorage.cartList = cartListString
+}
+
+const getLocalCartList = () => {
+  // 这里应该写成文档：{ shopId: {shopName: '', productList: { productId: {} } } }
+  return JSON.parse(localStorage.cartList) || {}
+}
+
 export default createStore({
   state: {
-    cartList: { // 这里应该写成文档：{ shopId: {shopName: '', productList: { productId: {} } } }
-      // shopId: {
-      //   shopName: '',
-      //   productList: {
-      //     productId: {
-      //       _id: '1'
-      //       ...
-      //     }
-      //   }
-      // }
-    }
+    cartList: getLocalCartList()
   },
   mutations: {
     changeCartItemInfo (state, payload) {
-      const { shopId, productId, productInfo } = payload
+      const { shopId, productId, productInfo, num } = payload
       const shopInfo = state.cartList[shopId] || {
         shopName: '', productList: {}
       }
@@ -25,11 +26,12 @@ export default createStore({
         product = productInfo
         product.count = 0
       }
-      product.count += payload.num
+      product.count += num
       if (payload.num > 0) { product.check = true }
       if (product.count < 0) { product.count = 0 }
       shopInfo.productList[productId] = product
       state.cartList[shopId] = shopInfo
+      setLocalCartList(state)
     },
     changeShopName (state, payload) {
       const { shopId, shopName } = payload
@@ -38,15 +40,18 @@ export default createStore({
       }
       shopInfo.shopName = shopName
       state.cartList[shopId] = shopInfo
+      setLocalCartList(state)
     },
     changeCartItemChecked (state, payload) {
       const { shopId, productId } = payload
       const product = state.cartList[shopId].productList[productId]
       product.check = !product.check
+      setLocalCartList(state)
     },
     cleanCartProducts (state, payload) {
       const { shopId } = payload
       state.cartList[shopId].productList = {}
+      setLocalCartList(state)
     },
     setCartItemsChecked (state, payload) {
       const { shopId } = payload
@@ -57,6 +62,7 @@ export default createStore({
           product.check = true
         }
       }
+      setLocalCartList(state)
     }
   },
   actions: {
