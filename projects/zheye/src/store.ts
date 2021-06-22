50a34782de5b1e918eb0b76699c4489410326ea5
LHJ Piper <2940596912@qq.com>
2021-06-22 09:40:22 +0800

    vue3 ts proj route guard

--- projects/zheye/src/store.ts
+++ projects/zheye/src/store.ts
@@ -1,23 +1,82 @@
-import { createStore } from 'vuex'
-
-const store = createStore({
-  state: {
-    user: { isLogin: false }
-  },
-  // mutation: {
-  //   login(state: { user: any }) {
-  //     state.user = { ...state.user, isLogin: true, name: 'viking' }
-  //   }
-  // }
-  getters: {
-    biggerColumnsLen(state) {
-      return state.columns.filter(c => c.id > 2).length
+import { createRouter, createWebHistory } from 'vue-router'
+import axios from 'axios'
+import Home from './views/Home.vue'
+import Login from './views/Login.vue'
+import Signup from './views/Signup.vue'
+import ColumnDetail from './views/ColumnDetail.vue'
+import CreatePost from './views/CreatePost.vue'
+import PostDetail from './views/PostDetail.vue'
+import store from './store'
+const routerHistory = createWebHistory()
+const router = createRouter({
+  history: routerHistory,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
     },
-    getColumnById: (state) => (id: number) => {
-      return state.columns.find(c => c.id === id)
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+      meta: { redirectAlreadyLogin: true }
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: Signup,
+      meta: { redirectAlreadyLogin: true }
+    },
+    {
+      path: '/create',
+      name: 'create',
+      component: CreatePost,
+      meta: { requiredLogin: true }
+    },
+    {
+      path: '/column/:id',
+      name: 'column',
+      component: ColumnDetail
+    },
+    {
+      path: '/posts/:id',
+      name: 'post',
+      component: PostDetail
     }
-    getPostsByCid: (state) => (cid: number) => {
-      return state.posts.filter(post => post.columnId === cid)
+  ]
+})
+router.beforeEach((to, from, next) => {
+  const { user, token } = store.state
+  const { requiredLogin, redirectAlreadyLogin } = to.meta
+  if (!user.isLogin) {
+    if (token) {
+      axios.defaults.headers.common.Authorization = `Bearer ${token}`
+      store.dispatch('fetchCurrentUser').then(() => {
+        if (redirectAlreadyLogin) {
+          next('/')
+        } else {
+          next()
+        }
+      }).catch(e => {
+        console.error(e)
+        store.commit('logout')
+        next('login')
+      })
+    } else {
+      if (requiredLogin) {
+        next('login')
+      } else {
+        next()
+      }
+    }
+  } else {
+    if (redirectAlreadyLogin) {
+      next('/')
+    } else {
+      next()
     }
   }
 })
+
+export default router
