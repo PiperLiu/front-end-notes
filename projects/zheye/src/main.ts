3b9539dc428f824d7db4b06d0c11140c61869a8d
LHJ Piper <2940596912@qq.com>
2021-06-24 15:44:51 +0800

    vue3 ts proj get into backend

--- projects/zheye/src/main.ts
+++ projects/zheye/src/main.ts
@@ -8,6 +8,16 @@ axios.defaults.baseURL = 'http://localhost:7001/api/'
 axios.interceptors.request.use(config => {
   store.commit('setLoading', true)
   store.commit('setError', { status: false, message: '' })
+  // get 请求，添加到 url 中
+  config.params = { ...config.params, icode: '34708A3A36A53FC5' }
+  // 其他请求，添加到 body 中
+  // 如果是上传文件，添加到 FormData 中
+  if (config.data instanceof FormData) {
+    config.data.append('icode', '34708A3A36A53FC5')
+  } else {
+    // 普通的 body 对象，添加到 data 中
+    config.data = { ...config.data, icode: '34708A3A36A53FC5' }
+  }
   return config
 })
 
