c7e515a5c512f5117fe89c85763b4c709bf8a87f
LHJ Piper <2940596912@qq.com>
2021-06-25 22:03:29 +0800

    vue3 ts proj: more about store.ts

--- projects/zheye/src/router.ts
+++ projects/zheye/src/router.ts
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import axios from 'axios'
 import Home from './views/Home.vue'
 import Login from './views/Login.vue'
 import Signup from './views/Signup.vue'
 import ColumnDetail from './views/ColumnDetail.vue'
 import CreatePost from './views/CreatePost.vue'
 import PostDetail from './views/PostDetail.vue'
+import store from './store'
 const routerHistory = createWebHistory()
 const router = createRouter({
   history: routerHistory,
@@ -43,6 +45,39 @@ const router = createRouter({
       component: PostDetail
     }
   ]
+})
+
+router.beforeEach((to, from, next) => {
+  const { user, token } = store.state
+  const { requiredLogin, redirectAlreadyLogin } = to.meta
+  if (!user.isLogin) {
+    if (token) {
+      axios.defaults.headers.common.Authorization = `Bearer ${token}`
+      store.dispatch('fetchCurrentUser').then(() => {
+        if (redirectAlreadyLogin) {
+          next('/')
+        } else {
+          next()
+        }
+      }).catch(e => {
+        console.error(e)
+        store.commit('logout')
+        next('login')
+      })
+    } else {
+      if (requiredLogin) {
+        next('login')
+      } else {
+        next()
+      }
+    }
+  } else {
+    if (redirectAlreadyLogin) {
+      next('/')
+    } else {
+      next()
+    }
+  }
 })
 
 export default router
